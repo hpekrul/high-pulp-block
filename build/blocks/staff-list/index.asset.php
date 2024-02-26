262e7d2316fe31d8c84142de5b348f321a5c090d
Holly Pekrul <hpekrul@my.wctc.edu>
2024-02-26 09:35:23 -0600

    added starter code.

---
+++ build/blocks/staff-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '2f05982fcd8d3cb933ad');
---
+++ build/blocks/staff-list/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7a58f0c35c14dd0f85ed');
