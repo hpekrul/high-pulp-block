dca9b56a3c7f819afc669e0122fbd302d0cdbb50
Holly Pekrul <hpekrul@my.wctc.edu>
2024-02-29 10:38:09 -0600

    patters demo in class

---
+++ patterns/pulpy-products.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Title: Pulpy Products
+ * Slug: high-pulp-blocks/pulpy-products
+ * Categories: featured, pulpy
+ *
+ * More headers can be found at:
+ * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"},"borderStyle":"solid"} -->
+<div class="wp-block-group has-border-color has-black-border-color"><!-- wp:heading -->
+	<h2 class="wp-block-heading">Pulpy Products</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:columns -->
+
+	<div class="wp-block-columns has-border-color has-black-border-color"><!-- wp:column -->
+		<div class="wp-block-column has-border-color has-black-border-color"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-resized has-custom-border"><img src="https://place-hold.it/150" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons"><!-- wp:button -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-border-color has-black-border-color wp-element-button">Buy Now</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:column -->
+
+		<div class="wp-block-columns has-border-color has-black-border-color"><!-- wp:column -->
+			<div class="wp-block-column has-border-color has-black-border-color"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full is-resized has-custom-border"><img src="https://place-hold.it/150" alt=""/></figure>
+				<!-- /wp:image -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons"><!-- wp:button -->
+					<div class="wp-block-button"><a class="wp-block-button__link has-border-color has-black-border-color wp-element-button">Buy Now</a></div>
+					<!-- /wp:button --></div>
+				<!-- /wp:buttons --></div>
+			<!-- /wp:column -->
+
+
+			<div class="wp-block-columns has-border-color has-black-border-color"><!-- wp:column -->
+				<div class="wp-block-column has-border-color has-black-border-color"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+					<figure class="wp-block-image size-full is-resized has-custom-border"><img src="https://place-hold.it/150" alt=""/></figure>
+					<!-- /wp:image -->
+
+					<!-- wp:buttons -->
+					<div class="wp-block-buttons"><!-- wp:button -->
+						<div class="wp-block-button"><a class="wp-block-button__link has-border-color has-black-border-color wp-element-button">Buy Now</a></div>
+						<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div>
+				<!-- /wp:column -->
+
+
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
