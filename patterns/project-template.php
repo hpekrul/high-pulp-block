20ffe69a6e51d720a35b299a185c9052be8069d1
Holly Pekrul <hpekrul@my.wctc.edu>
2024-03-03 16:40:40 -0600

    added project template pattern

---
+++ patterns/project-template.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Title: Project Template
+ * Slug: high-pulp-blocks/project-template
+ * Categories: featured, pulpy
+ *
+ * More headers can be found at:
+ * https://developer.wordpress.org/themes/features/block-patterns/#registering-block-patterns
+ */
+?>
+<!-- wp:group {"backgroundColor":"cyan-bluish-gray","layout":{"type":"constrained"},"metadata":{"name":"Project Template"}} -->
+<div class="wp-block-group has-cyan-bluish-gray-background-color has-background"><!-- wp:heading -->
+	<h2 class="wp-block-heading">Projects </h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:columns -->
+	<div class="wp-block-columns"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-resized"><img src="https://place-hold.it/300" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"level":4} -->
+			<h4 class="wp-block-heading">Title</h4>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Description</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red"} -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button">View More</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-resized"><img src="https://place-hold.it/300" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"level":4} -->
+			<h4 class="wp-block-heading">Title</h4>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Description</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red"} -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button">View More</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns -->
+	<div class="wp-block-columns"><!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-resized"><img src="https://place-hold.it/300" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"level":4} -->
+			<h4 class="wp-block-heading">Title</h4>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Description</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red"} -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button">View More</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full is-resized"><img src="https://place-hold.it/300" alt=""/></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:heading {"level":4} -->
+			<h4 class="wp-block-heading">Title</h4>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Description</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"vivid-red"} -->
+				<div class="wp-block-button"><a class="wp-block-button__link has-vivid-red-background-color has-background wp-element-button">View More</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons --></div>
+		<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+<!-- /wp:group -->
